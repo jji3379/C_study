b8fdf441a375ef8cc66670977d46d54192e3105c
jji3379 <jji3379@naver.com>
2021-05-24 17:25:56 +0900

    Initial Commit

---
+++ C_study/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_study
+//
+//  Created by users on 5/24/3 R.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
