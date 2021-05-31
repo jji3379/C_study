646cc66c670042f5dc7695a8d7ddfffc27731a6f
jji3379 <jji3379@naver.com>
2021-05-31 22:29:27 +0900

    Operator ( 다른 언어랑 다를거 없음 )

---
+++ C_study/Operator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    //arithmatic operation
+    // +, - , *, %, ++, --
+    
+    //relational operation
+    // ==, !=, >, <, >=, <=
+    int a=1;
+    int b=5;
+    printf("a==b : %d\n", a==b );
+    printf("a!=b : %d\n", a!=b );
+    printf("a>b : %d\n", a>b );
+    printf("a<b : %d\n", a<b );
+    printf("a>=b : %d\n", a>=b );
+    printf("a<=b : %d\n", a<=b );
+    
+    //bit-wise operation
+    // &, |, ^, ~, <<, >>
+    
+    //logical operation
+    // &&(AND), ||(OR), !(NOT)
+    int man=1;
+    int age=1;
+    int inkorea=1;
+    
+    printf("%d", man && age && inkorea);
+    
+    //assignment(배정) operation
+    // =, +=, -=, *=, /=, %=
+    
+    return 0;
+}
